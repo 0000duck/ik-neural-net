b3cde775881aec8daf7e029442297d544a4b55fb
Joao Gaspar Cunha <gasparcunha19@outlook.pt>
2021-02-11 18:41:10 +0000

    personal DeepLearning files

---
+++ Python/JoaoCunha/test.py
@@ -0,0 +1,20 @@
+import mnist_loader
+import network
+from numba import jit, cuda 
+
+""" read the input data """
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+""" Initialize the network """
+# test1
+#net = network.Network([2, 3, 1])
+
+# test2
+#net = network.Network([784, 30, 10])
+
+# test3
+net = network.Network([784, 100, 10])
+
+""" Train the network using Stochastic Gradient Descent """
+# SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None)
+net.SGD(training_data , 30, 10, 3.0, test_data=test_data)
